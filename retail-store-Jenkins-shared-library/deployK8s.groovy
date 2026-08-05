c8c26d2a0f236b669307319e5a9f02587f5c2c5e
ahmad24amliwala <ahmad24amliwala@gmail.com>
2026-08-05 23:15:05 +0530

    refactor: improve GitOps deployment pipeline

--- retail-store-Jenkins-shared-library/deployK8s.groovy
+++ retail-store-Jenkins-shared-library/deployK8s.groovy
@@ -1,27 +1,23 @@
 def call(Map config) {
 
-    sh """
-    echo "🚀 Deploying ${config.service} version ${config.version}"
+    dir('.') {
 
-    git clone https://github.com/Sarthakx67/retail-store-aws-deployment.git
-    cd retail-store-aws-deployment
+        sh """
+        echo "Deploying ${config.service} ${config.version}"
 
-    sed -i "s/tag: v1/tag: ${config.version}/" \
-    retail-store-helm-chart/values/${config.env}/values-${config.env}.yaml
+        yq -i '
+        .retail-store-${config.service}.image.tag = "${config.version}"
+        ' retail-store-helm-chart/values/prod/values-prod.yaml
 
-    git config user.email "jenkins@ci.com"
-    git config user.name "Jenkins"
-    git add .
-    git commit -m "ci: update ${config.service} to ${config.version}"
-    git push
-    
-    """
+        git config user.email "jenkins@ci.com"
+        git config user.name "Jenkins CI"
+
+        git add retail-store-helm-chart/values/prod/values-prod.yaml
+
+        git commit -m "ci: deploy ${config.service}:${config.version}" || true
+
+        git push origin main
+        """
+    }
+
 }
-    // update the exact key in the exact file
-    // ```
-    // ArgoCD detects the Git change within 3 minutes, syncs automatically, deploys v2. No drift, no fighting.
-    // ---
-    // **The pattern has a name — Image Updater or GitOps Push:**
-    // ```
-    // Jenkins:  build image → push DockerHub → update Git → push
-    // ArgoCD:   detects Git change → syncs cluster
